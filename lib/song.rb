ae3604b31d00c73b609a91446c082249bca1ee12
Austin Rhoads <austinrrhoads@ymail.com>
2020-03-02 01:05:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,8 +19,8 @@ end
 def self.new_by_filename(name)
 file = name.split(" - ")
   song = Song.new(file[1])
-  artist = Artist.find_or_create_by_name(file[0])
-  song.artist = artist
+  song.artist = Artist.find_or_create_by_name(file[0])
+  song.artist.add_song << song
   song
 end
 
